9aefcacbe1b9a085260110f8a4ccb534b931e61a
TekGamerz <86511016+TekGamerz@users.noreply.github.com>
2021-08-19 12:37:55 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,14 @@
+<?php
+session_start();
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+ 
+function loginForm(){
+    echo'>
